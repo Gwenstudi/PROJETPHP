ebc3b288c3440cdd79832f4b41ecf9486626acee
Gwenaelstudi <g.goisbeaut@gmail.com>
2024-02-23 14:45:35 +0100

    xc

--- Bouclefor/jeux de caracères2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$g = readline ("Entrez une phrase : "); 
-$cle = readline ("Entrez la clé : ");  
-$tableau = str_split ($g); //particularite php pour découper une chaine de caractère
-foreach ($tableau as $g) {// supprimer le douvle / pour les autres exemples
-}   
